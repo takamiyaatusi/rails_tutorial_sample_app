abc49720520155ea9a6e3f2b106aef9409543e4d
kosuke <nakamuratetuhito@gmail.com>
2014-01-14 22:04:56 +0900

    FinishStaticPages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "Home page" do
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+
+  describe "About page" do
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+    end
+  end
+
+end
