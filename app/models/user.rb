6b693a3f03e1db333c6d911e76586fe9c6178829
kosuke <nakamuratetuhito@gmail.com>
2014-02-16 15:15:16 +0900

    sign-in-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { email.downcase! }
+  before_create :create_remember_token
 
   validates(:name, presence: true, length:{maximum:50})
 
@@ -13,4 +14,20 @@ class User < ActiveRecord::Base
              length: { minimum: 6 }
 
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+
 end
